312b01e61c58781e73158dbd2a255e5668581fc3
Serdar Demirci <o_serdardemirci@hotmail.com>
2021-05-24 16:08:15 +0200

    initial commit

---
+++ src/test/java/com/serdardemirci/databaserelationship/DatabaserelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serdardemirci.databaserelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaserelationshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
